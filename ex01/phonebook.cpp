c36581d76a35857a1c9ba1a7213a5628f75c4e1c
kirabin <kirabin.one@gmail.com>
2021-05-03 18:05:21 +0300

    Day 1

---
+++ ex01/phonebook.cpp
@@ -0,0 +1,15 @@
+#include "phonebook.hpp"
+
+int	main(void)
+{
+	int		i = 1;
+
+	while (i)
+	{
+		cout << "Enter number: ";
+		cin >> i;
+		cout << "Your number: " << i << endl;
+	}
+
+	return 0;
+}
---
+++ ex01/phonebook.hpp
@@ -0,0 +1,18 @@
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+# include <iostream>
+# include <string.h>
+# include <iomanip>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+// enum Command {exit, add, search};
+
+// enum Add_field {first_name, last_name, nickname, login, postal_address, email_address, phone_number, birthday, date, favorite_meal, underwear_color, darkest_secret};
+
+// enum Search_field {index, first_name, last_name, nickname};
+
+
+#endif
